19920e7ce8ef54291e1c786b7e060cc53b139ae8
DongheeL <dnghlee19@gmail.com>
2022-10-18 18:02:34 +0900

    rotateY효과 추가, setState 안됨 확인 필요

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button, ImageList, ImageListItem } from '@mui/material';
+import { Button, cardHeaderClasses, ImageList, ImageListItem } from '@mui/material';
 import { stat } from 'fs';
 import zIndex from '@mui/material/styles/zIndex';
 
 
 function App() {
+  
+  type card = {
+    no:number,
+    isFlip:boolean
+  }
 
   const [level,setLevel] = useState<number>(1);
-  const [cards,setCard] = useState<{no:number,isFlip:boolean}[]>([]);
+  const [cards,setCard] = useState<card[]>([]);
 
-  function getNewCards(number:number){
+  useEffect(()=>{
+    console.log(cards)
+  },[cards])
+
+  const getNewCards = (number:number)=>{
     setLevel(number);
     let total = Math.pow(2*number,2);
-    let icons:{no:number,isFlip:boolean}[] = [];
+    let icons:card[] = [];
     let numbers:number[] = [];
 
     for(let i=0;i<total/2;i++){
@@ -23,17 +32,33 @@ function App() {
         i--;
       }else{
         icons.push({no:tmp,isFlip:true});
-        icons.push({no:tmp,isFlip:false});
+        icons.push({no:tmp,isFlip:true});
         numbers.push(tmp);
       }
     }
     icons.sort(()=>Math.random()-0.5);
-    console.log(icons);
+    console.log(icons)
+
     setCard(icons);
+    let n: any;
+    n = setTimeout(function(){flipAll()}, 1000);
   }
 
-  function setFlip(index:number){
-    // cards.
+  const flipAll=()=>{
+    console.log(cards);
+    const newCard = [...cards];
+    newCard.map((val)=>val.isFlip=false);
+    setCard(newCard);
+  }
+
+  const setFlip = (no:number)=>{
+    //useState로 상태가 변경되는 배열을 수정할 경우,
+    //state 변경함수들은 state를 완전히 대체하는 것이기 때문에 완전히 새로운 배열을 넣어주어야 함.
+    //따라서, 기존 배열을 수정해도 변경되지 않음.
+    //기존 배열을 복사한 새로운 배열을 수정한 후 state변경 함수에 넣어주어야 새로운 배열로 적용됨.
+    const newCard = [...cards];
+    newCard[no].isFlip = !newCard[no].isFlip;
+    setCard(newCard);
   }
 
   return (
@@ -64,8 +89,8 @@ function App() {
             return(
               <ImageListItem key={index} style={{ maxWidth:`${100}%`, height:'100%'}}  >
                 <div className='card' style={{ maxWidth:`${100}%`, height:'100%'}} onClick={()=>setFlip(index)}>
-                  {val.isFlip ?
-                    <div className= 'front'>
+                  {/* {val.isFlip ? */}
+                    <div className= {`front ${val.isFlip ? '':'isFlip'}`}>
                       <img  
                         // className='back'
                         style={{width:`${700/(2*level)*0.9}px`}}
@@ -75,11 +100,11 @@ function App() {
                         key={index}
                       />                    
                     </div>
-                  : 
-                  <div className= 'back' style={{width:`${700/(2*level)*0.9}px`, height:`${700/(2*level)*0.9}px`}}>
+                  {/* :  */}
+                  <div className= {`back ${val.isFlip ? '':'isFlip'}`} style={{width:`${700/(2*level)*0.9}px`, height:`${700/(2*level)*0.9}px`}}>
                     back
                   </div>
-                }
+                {/* } */}
               </div>
               </ImageListItem>  
             )
