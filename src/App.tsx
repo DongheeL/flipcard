96b965acc124f3e98221b0559323463ff3f83434
DongheeL <dnghlee19@gmail.com>
2023-03-23 16:53:25 +0900

    주석 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useRef } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Button, cardHeaderClasses, ImageList, ImageListItem } from '@mui/material';
 import { stat } from 'fs';
